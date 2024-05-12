88de271b888701a0a251ce8fb42705c8cfa26eef
linuxdaemon <linuxdaemon.irc@gmail.com>
2024-05-12 15:06:31 -0400

    feat: dump task info

--- src/gvm_sync_targets/cli/__init__.py
+++ src/gvm_sync_targets/cli/__init__.py
@@ -1,12 +1,17 @@
 # SPDX-FileCopyrightText: 2024-present linuxdaemon <linuxdaemon.irc@gmail.com>
 #
 # SPDX-License-Identifier: MIT
+from typing import TYPE_CHECKING, Optional, TextIO
+
 import click
 from gvm.connections import UnixSocketConnection
 from gvm.protocols.gmp import Gmp
 from gvm.transforms import EtreeCheckCommandTransform
 
 from gvm_sync_targets import __version__
+
+if TYPE_CHECKING:
+    from lxml.etree import ElementBase
 
 
 @click.group(
@@ -19,11 +24,40 @@ from gvm_sync_targets import __version__
 @click.version_option(version=__version__, prog_name="gvm-sync-targets")
 @click.option("--username", show_envvar=True)
 @click.option("--password", show_envvar=True)
-def gvm_sync_targets(username: str, password: str) -> None:
+@click.argument("hosts_file", type=click.File())
+def gvm_sync_targets(username: str, password: str, hosts_file: TextIO) -> None:
     with Gmp(
         UnixSocketConnection(), transform=EtreeCheckCommandTransform()
     ) as gmp:
+        hosts = hosts_file.read().splitlines()
         gmp.authenticate(username, password)
-        targets = gmp.get_targets()
-        print(targets)
-        print(type(targets))
+        targets: "ElementBase" = gmp.get_targets()
+        target = targets.find("/get_targets_response/target[name='All Hosts']")
+
+        if target is None:
+            # Create target
+            new_target = gmp.create_target("All Hosts")
+        elif target.xpath("/in_use/text()") == ["1"]:
+            new_target = gmp.clone_target(target.attrib["id"])
+        else:
+            new_target = target
+
+        # Set hosts
+        # gmp.modify_target(new_target.attrib['id'], hosts=hosts)
+
+        # Replace old target in tasks
+        if target is not None and target is not new_target:
+            tasks = gmp.get_tasks().findall("/get_tasks_response/task")
+            if tasks is None:
+                tasks = []
+
+            for task in tasks:
+                print(task)
+                print(task.attrib)
+                for child in task:
+                    print(child)
+                    print(child.attrib)
+
+            # Ensure old target is unused
+            # Delete old target
+            # Rename new target
